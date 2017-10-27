3892ee29f69a173d856f62cda44246a53cdec7de
Pythonify <qingxuanshabao@qq.com>
2017-10-27 19:50:54 +0800

    refactor main cmake file

--- cmake_install.cmake
+++ cmake_install.cmake
@@ -29,9 +29,7 @@ endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
-  include("/Users/lichenxi/ClionProjects/jvm/libs/libzip/cmake_install.cmake")
   include("/Users/lichenxi/ClionProjects/jvm/libs/docopt/cmake_install.cmake")
-  include("/Users/lichenxi/ClionProjects/jvm/src/cmake_install.cmake")
 
 endif()
 
